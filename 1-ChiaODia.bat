b96e265023cd129d53cf21aab91ea9f335217703
minhtuan283 <tanghuynhminhtuan@gmail.com>
2025-02-23 14:51:46 +0700

    Create 1-ChiaODia.bat

---
+++ 1-ChiaODia.bat
@@ -0,0 +1,7 @@
+@echo OFF
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+xcopy /e %~dp0soft C:\Windows
+
+start C:\Windows\Soft\Auto-MT.bat
+exit
