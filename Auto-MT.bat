e634aa6e69ad1e150527bfc846919a1ff7b5cc8a
minhtuan283 <tanghuynhminhtuan@gmail.com>
2025-03-11 16:01:27 +0700

    Update Auto-MT.bat

--- Auto-MT.bat
+++ Auto-MT.bat
@@ -1,17 +1,18 @@
 @echo OFF
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-rmdir /s /q "%~dp0\App" 2>nul
-mkdir "%~dp0\App"
-"%~dp0\Tools\7z.exe" x "%~dp0App.zip" -o"%~dp0\App" -y -aoa
 rmdir /s /q "%~dp0\Script" 2>nul
 mkdir "%~dp0\Script"
 "%~dp0\Tools\7z.exe" x "%~dp0Script.zip" -o"%~dp0\Script" -pminhtuan283 -y -aoa
+ping 127.0.0.1 -n 2
+diskpart.exe /s %~dp0\Script\doiten.txt
+ping 127.0.0.1 -n 2
+rmdir /s /q "%~dp0\App" 2>nul
+mkdir "%~dp0\App"
+"%~dp0\Tools\7z.exe" x "%~dp0App.zip" -o"%~dp0\App" -y -aoa
 del /q "%~dp0App.zip" >nul 2>&1
 del /q "%~dp0Script.zip" >nul 2>&1
-ping 127.0.0.1 -n 1
-diskpart.exe /s %~dp0\Script\doiten.txt
-ping 127.0.0.1 -n 3
+
 rmdir /s /q "%USERPROFILE%\desktop\1-Soft"
 start "" /min "%~dp0Script\internet.bat"
 cls
